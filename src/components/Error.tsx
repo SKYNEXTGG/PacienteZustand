bde5e562ddf19956cbc95f73c986bb6f61cf4759
SKYNEXTG <tu_email@example.com>
2024-09-04 15:24:18 -0700

    Version Completa

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,7 +1,8 @@
-export default function Error({ children }: { children: React.ReactNode }) {
+export default function Error({children} : {children: React.ReactNode}) {
     return (
-        <p className="text-center my-4 bg-red-600 text-white font-bold p-3 uppercase text-sm">
-            {children}
-        </p>
+      <p className="text-center my-4 bg-red-600 text-white font-bold p-3 uppercase text-sm">
+          {children}
+      </p>
     )
-}
+  }
+  
